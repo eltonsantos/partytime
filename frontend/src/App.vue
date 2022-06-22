55e6edea81bd60a935c53f3f626fe6269ff8f44e
Elton Santos <eltonaxl@hotmail.com>
2022-06-22 16:30:31 -0300

    Almost finish

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Navbar />
+    <Message />
     <router-view class="container" />
     <Footer />
   </div>
@@ -9,16 +10,32 @@
 <script>
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-
+import Message from './components/Message';
 export default {
   name: "App",
   components: {
     Navbar,
-    Footer
+    Footer,
+    Message
   }
 }
-
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Helvetica;
+    color: #444;
+  }
+  .container {
+    min-height: 300px;
+  }
+  a {
+    transition: .5s;
+  }
+  a:hover {
+    color: #c1b696;
+  }
 </style>
--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-  Rodapé
+  <footer>
+    <h3>Party Time &copy; 2022</h3>
+    <p>O melhor local para compartilhar seus eventos!</p>
+  </footer>
 </template>
 
 <script>
@@ -7,3 +10,23 @@ export default {
   name: "Footer"
 }
 </script>
+
+<style scoped>
+  footer {
+    background-color: #25282e;
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  footer h3 {
+    color: #FFF;
+    margin-bottom: 15px;
+    font-weight: 300;
+    font-size: 1.6rem;
+  }
+  footer p {
+    color: #c1b696;
+  }
+</style>
